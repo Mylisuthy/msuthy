f22cfc4ff3279a4e3e311a5bb1f9b8847f6f1247
Mylisuthy <juanch052048@gmail.com>
2025-04-22 13:41:18 -0500

    Update entrenamiento.py
    lol

--- entrenamiento.py
+++ entrenamiento.py
@@ -1,8 +1,53 @@
+# 1) invertir valores
+a = float(input("Dame dame un numero "))
+b = float(input("Dame el otro uwu "))
+print(f"Sus numeros son {a}, y {b}")
+a,b = b, a
+print (f"Sus numeros invertidos son {a}, {b}")
 
-nombre = input("tu nombre:")
+# 2) invertir num 3 cifras
+#se piden los datos al usuario
+numeros = int(input("dame un numero de 3 cifras"))
+#transforma la variable numeros en caracteres, la enlista y lee la lista hacia atras 
+inver = int(str(numeros)[::-1])
+#escribe el numero ya transformado y llama variable
+print(f"el numero es" + str(inver))
+#hace las operaciones y transforma las variables
+centena = int(inver) //100
+Decena = (int(inver) % 100) // 10
+Unidades = int(inver) % 10
+#lee, escribe y organiza los datos
+print(f"estas son tus centenas:{centena}\nestas tus decenas: {Decena}\nestas tus unidades: {Unidades}")
 
-apellido = input ("tu apellido")
+# 3) calculo de timpo en segundos
+#se pide el dato a usar
+numero = int(input("dime un numero para el calculo"))
+#variables y calculo de operaciones
+hora = int(numero) // 3600
+minutos = int(numero) %3600 // 60
+segundos = int(numero) % 60
+#escribe/inprime el resultado de las variables con un mensage pre definido
+print (f"tienes {hora} hora's, {minutos} minutos y {segundos} segundos")
 
-edad = input("edad")
+# 4) pedir fecha al usuario declarando variable
+#peticion de datos
+dd = int(input("dame el numero del mes en el que nacimiento " ))
+mm = int(input("dame el dia en que naciste " ))
+aaaa = int(input("dame el año en que naciste " ))
+#transformar variable (.zfill = rellena el espacio basio con 0)
+dd = str(dd).zfill(2)
+mm = str(mm).zfill(2)
+#escribir texto, mostrar datos
+print(f" Naciste el {dd}/{mm}/{aaaa} \n La fecha al contrario {aaaa}-{mm}-{dd}")
 
-print(f"hola soy {nombre} {apellido} y tengo {edad} de edad")
+# 5) converción de temperatura 
+# Solicitar la temperatura en Fahrenheit
+Temperatura = input("Dame tu temperatura en Fahrenheit: ")
+
+# Verificar si la entrada es un número
+if Temperatura.replace('.', '', 1).isdigit() or (Temperatura[0] == '-' and Temperatura[1:].replace('.', '', 1).isdigit()):
+    Temperatura = float(Temperatura)  # Convertir a float si es un número válido
+    temperatura_c = (Temperatura - 32) / 1.8
+    print(f"Su temperatura en Fahrenheit: {Temperatura} \nSu temperatura en Celsius: {temperatura_c}")
+else:
+    print("La temperatura se mide con números, ¿no?")
